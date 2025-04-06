b4e85a5dc0b6dda4f37f2de54061f1ed66726fd8
Saugat <saugatpokharel201@gmail.com>
2025-04-06 14:30:31 +0545

    JRadio Button

---
+++ Java_Swing/src/JRadioButtons.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+class MyFrame extends JFrame implements ActionListener{
+JRadioButton pizzaButton,hamburgerButton,hotdogButton;
+MyFrame(){
+    this.setSize(520,520);
+    this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+    this.setTitle("JCheckBox");
+    this.setLayout(new FlowLayout());
+    // this.pack();
+    pizzaButton=new JRadioButton("pizza");
+    hamburgerButton=new JRadioButton("hamburger");
+    hotdogButton=new JRadioButton("hotdog");
+    ButtonGroup group=new ButtonGroup();
+    //adding action listiner to each button
+    pizzaButton.addActionListener(this);
+    hamburgerButton.addActionListener(this);
+    hotdogButton.addActionListener(this);
+    group.add(pizzaButton);
+    group.add(hamburgerButton);
+    group.add(hotdogButton);
+    this.add(pizzaButton);
+    this.add(hamburgerButton);
+    this.add(hotdogButton);
+    this.setVisible(true);
+}
+public void actionPerformed(ActionEvent e){
+if(e.getSource()==pizzaButton){
+    System.out.println("Pizza in the House");
+}
+if(e.getSource()==hamburgerButton){
+    System.out.println("Hamburger in the house");
+}
+if(e.getSource()==hotdogButton){
+    System.out.println("Hotdog in the house");
+}
+}
+}
+
+
+public class JRadioButtons {
+    public static void main(String[] args) {
+        new MyFrame();
+     } 
+}
